2600686a014d195a1d9c8ad3de34bf83d561abb0
AbdulRehman2005101 <abdulrehmanamjad2005101@gmail.com>
2025-10-16 15:09:06 +0500

    course model updated

--- Backend/models/Course.js
+++ Backend/models/Course.js
@@ -13,7 +13,52 @@ const courseSchema = new mongoose.Schema({
   taIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   studentIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+// Virtual populate for assignments
+courseSchema.virtual('assignments', {
+  ref: 'Assignment',
+  localField: '_id',
+  foreignField: 'courseId'
+});
+
+// Virtual populate for announcements
+courseSchema.virtual('announcements', {
+  ref: 'Announcement',
+  localField: '_id',
+  foreignField: 'courseId'
+});
+
+// Virtual populate for lectures
+courseSchema.virtual('lectures', {
+  ref: 'Lecture',
+  localField: '_id',
+  foreignField: 'courseId'
+});
+
+// Virtual populate for teacher
+courseSchema.virtual('teacher', {
+  ref: 'User',
+  localField: 'teacherId',
+  foreignField: '_id',
+  justOne: true
+});
+
+// Virtual populate for TAs
+courseSchema.virtual('tas', {
+  ref: 'User',
+  localField: 'taIds',
+  foreignField: '_id'
+});
+
+// Virtual populate for students
+courseSchema.virtual('students', {
+  ref: 'User',
+  localField: 'studentIds',
+  foreignField: '_id'
 });
 
 module.exports = mongoose.model('Course', courseSchema);
